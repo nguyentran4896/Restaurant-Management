b4ab46613bf4c18f8e784ca5be178d79fd377351
nguyentran4896 <nguyentran4896@gmail.com>
2017-12-11 20:38:47 +0700

    Fix order detail

--- src/components/pages/order-detail.js
+++ src/components/pages/order-detail.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 class OrderDetail extends Component {
   render () {
@@ -10,19 +11,17 @@ class OrderDetail extends Component {
               <table className='table cart'>
                 <thead>
                   <tr>
-                    <th className='cart-product-remove'>&nbsp;</th>
                     <th className='cart-product-thumbnail'>&nbsp;</th>
                     <th className='cart-product-name'>Món</th>
                     <th className='cart-product-price'>Đơn giá</th>
                     <th className='cart-product-quantity'>Số lượng</th>
                     <th className='cart-product-subtotal'>Tổng</th>
+                    <th className='cart-product-name'>Trạng thái</th>
+                    <th className='cart-product-subtotal'>Hủy?</th>
                   </tr>
                 </thead>
                 <tbody>
                   <tr className='cart_item'>
-                    <td className='cart-product-remove'>
-                      <a href='#' className='remove' title='Remove this item'><i className='icon-trash2' /></a>
-                    </td>
 
                     <td className='cart-product-thumbnail'>
                       <a href='#'><img width='64' height='64' src={require('images/restaurant/food-3.jpg')} alt='Pink Printed Dress' /></a>
@@ -47,11 +46,16 @@ class OrderDetail extends Component {
                     <td className='cart-product-subtotal'>
                       <span className='amount'>$39.98</span>
                     </td>
-                  </tr>
-                  <tr className='cart_item'>
-                    <td className='cart-product-remove'>
+
+                    <td className='cart-product-name'>
+                      <span class='label label-success'>Đã gọi</span>
+                    </td>
+
+                    <td className='cart-product-remove text-center'>
                       <a href='#' className='remove' title='Remove this item'><i className='icon-trash2' /></a>
                     </td>
+                  </tr>
+                  <tr className='cart_item'>
 
                     <td className='cart-product-thumbnail'>
                       <a href='#'><img width='64' height='64' src={require('images/restaurant/2.jpg')} alt='Checked Canvas Shoes' /></a>
@@ -76,11 +80,15 @@ class OrderDetail extends Component {
                     <td className='cart-product-subtotal'>
                       <span className='amount'>$24.99</span>
                     </td>
-                  </tr>
-                  <tr className='cart_item'>
-                    <td className='cart-product-remove'>
+
+                    <td className='cart-product-name'>
+                      <span class='label label-success'>Đã gọi</span>
+                    </td>
+                    <td className='cart-product-remove text-center'>
                       <a href='#' className='remove' title='Remove this item'><i className='icon-trash2' /></a>
                     </td>
+                  </tr>
+                  <tr className='cart_item'>
 
                     <td className='cart-product-thumbnail'>
                       <a href='#'><img width='64' height='64' src={require('images/restaurant/1.jpg')} alt='Pink Printed Dress' /></a>
@@ -105,9 +113,16 @@ class OrderDetail extends Component {
                     <td className='cart-product-subtotal'>
                       <span className='amount'>$41.97</span>
                     </td>
+
+                    <td className='cart-product-name'>
+                      <span class='label label-danger'>Đã hủy</span>
+                    </td>
+                    <td className='cart-product-remove text-center'>
+                      <a href='#' className='remove' title='Remove this item'><i className='icon-trash2' /></a>
+                    </td>
                   </tr>
                   <tr className='cart_item'>
-                    <td colSpan='6'>
+                    <td colSpan='7'>
                       <div className='row clearfix'>
                         <div className='col-md-4 col-xs-4 nopadding'>
                           <div className='col-md-8 col-xs-7 nopadding'>
@@ -120,6 +135,7 @@ class OrderDetail extends Component {
                         <div className='col-md-8 col-xs-8 nopadding'>
                           <a href='#' className='button button-3d nomargin fright'>Thanh toán ngay</a>
                           <a href='#' className='button button-3d notopmargin fright'>Gọi món</a>
+                          <Link to='/menu' className='button button-3d notopmargin fright button-green'>Gọi thêm</Link>
                         </div>
                       </div>
                     </td>
