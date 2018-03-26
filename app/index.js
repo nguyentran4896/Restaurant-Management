9cd43bb1efa634063bad83d5ca9f6b7a076f8640
nguyentran4896 <nguyentran4896@gmail.com>
2018-03-26 16:55:16 +0700

    Fixed route

--- app/index.js
+++ app/index.js
@@ -1,41 +1,41 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Router, Route, IndexRoute } from 'react-router'
 import { Provider } from 'react-redux'
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom'
 
 import Navigator from 'lib/Navigator'
 import Store from 'lib/Store'
-import App from 'App'
-import Login from 'pages/Login'
-import Dashboard from 'pages/Dashboard'
-import EmployeeList from 'components/admin/employees/EmployeeList'
-import EmployeeView from 'components/admin/employees/EmployeeView'
-import EmployeeEdit from 'components/admin/employees/EmployeeEdit'
-import EmployeeCreate from 'components/admin/employees/EmployeeCreate'
-
-import FoodCategoryList from 'components/admin/foodCategories/FoodCategoryList'
-import FoodCategoryView from 'components/admin/foodCategories/FoodCategoryView'
-import FoodCategoryEdit from 'components/admin/foodCategories/FoodCategoryEdit'
-import FoodCategoryCreate from 'components/admin/foodCategories/FoodCategoryCreate'
+import Header from 'components/header/header.js'
+import HomeHeader from 'components/header/home-header.js'
+import Footer from 'components/footer/footer.js'
+import Homepage from 'components/pages/home.js'
+import Menu from 'components/pages/menu.js'
+import Event from 'components/pages/event.js'
+import Contact from 'components/pages/contact.js'
+import Auth from 'components/pages/auth.js'
 
 ReactDOM.render((
   <Provider store={Store}>
     <Router history={Navigator}>
-      <Route path='/' component={App}>
-        <IndexRoute component={Login} />
-        <Route path='login' component={Login}/>
-        <Route path='dashboard' component={Dashboard} />
+      <Router>
+        <div className='App'>
+          <Switch>
+            <Route path='/' component={HomeHeader} exact />
+            <Route path='*' component={Header} />
+          </Switch>
 
-        <Route path='employees' component={EmployeeList} />
-        <Route path='employee-view' component={EmployeeView} />
-        <Route path='employee-edit' component={EmployeeEdit} />
-        <Route path='employee-create' component={EmployeeCreate} />
-
-        <Route path='food-categories' component={FoodCategoryList} />
-        <Route path='food-category-view' component={FoodCategoryView} />
-        <Route path='food-category-edit' component={FoodCategoryEdit} />
-        <Route path='food-category-create' component={FoodCategoryCreate} />
-      </Route>
+          <Route exact path='/' component={Homepage} />
+          <Route exact path='/menu' component={Menu} />
+          <Route exact path='/event' component={Event} />
+          <Route exact path='/contact' component={Contact} />
+          <Route exact path='/auth' component={Auth} />
+          <Footer />
+        </div>
+      </Router>
     </Router>
   </Provider>
 ), document.getElementById('website'))
