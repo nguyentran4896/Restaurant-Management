213eba8e48aa6ece4f96630641a6043e2588515e
Diep Phong <winterspringvnt@gmail.com>
2018-04-17 01:35:25 +0700

    add contact submit

--- app/components/pages/contact.js
+++ app/components/pages/contact.js
@@ -1,6 +1,54 @@
 import React, { Component } from 'react'
+import request from 'request-promise'
+
+import { makeRequestOptions } from '../../lib/requestHeader'
+import { showNotification } from '../../lib/actions/showNotification'
 
 class Contact extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      name: '',
+      phoneNumber: '',
+      address: '',
+      email: '',
+      message: ''
+    };
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handlePhoneNumberChange = this.handlePhoneNumberChange.bind(this);
+    this.handleAddressChange = this.handleAddressChange.bind(this);
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handleMessageChange = this.handleMessageChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+  
+  
+  handleNameChange(evt){
+    this.setState({name: evt.target.value});
+  }
+  handlePhoneNumberChange(evt){
+    this.setState({phoneNumber: evt.target.value});
+  }
+  handleAddressChange(evt){
+    this.setState({address: evt.target.value});
+  }
+  handleEmailChange(evt){
+    this.setState({email: evt.target.value});
+  }
+  handleMessageChange(evt){
+    this.setState({message: evt.target.value});
+  }
+  onSubmit(){
+    var name= this.state.name;
+    debugger;
+    request(makeRequestOptions(this.state, 'website/contact')).then(body=>{
+      if(body.code === 0){
+        showNotification('topRight', 'success', 'Cảm ơn bạn đã liên hệ!')
+      } else {
+        showNotification('topRight', 'error', 'Đã có lỗi xảy ra!')
+      }
+    });
+  }
   render () {
     return (
       <div className='contact'>
@@ -33,33 +81,33 @@ class Contact extends Component {
             </div>
             <div className='col-md-7 contact-para animated wow fadeInDown' data-wow-duration='1000ms' data-wow-delay='500ms'>
               <h5>Contact Form</h5>
-              <form>
+              <form >
                 <div className='grid-contact'>
                   <div className='col-md-6 contact-grid'>
                     <p className='your-para'>Name </p>
-                    <input type='text' defaultValue='' />
+                    <input type='text' name='name' onChange={this.handleNameChange} />
                   </div>
                   <div className='col-md-6 contact-grid'>
                     <p className='your-para'>Phone number</p>
-                    <input type='text' defaultValue='' />
+                    <input type='text' name='phoneNumber' onChange={this.handlePhoneNumberChange}  />
                   </div>
                   <div className='clearfix' />
                 </div>
                 <div className='grid-contact'>
                   <div className='col-md-6 contact-grid'>
-                    <p className='your-para'>Email</p>
-                    <input type='text' defaultValue='' />
+                    <p className='your-para'>Address</p>
+                    <input type='text'  name='address' onChange={this.handleAddressChange}  />
                   </div>
                   <div className='col-md-6 contact-grid'>
-                    <p className='your-para'>Password</p>
-                    <input type='Password' defaultValue='' />
+                    <p className='your-para'>Email</p>
+                    <input type='email'  name='email' onChange={this.handleEmailChange}  />
                   </div>
                   <div className='clearfix' />
                 </div>
                 <p className='your-para msg-para'>MESSAGE</p>
-                <textarea cols='77' rows='6' defaultValue='' />
+                <textarea cols='77' name='message'  rows='6' onChange={this.handleMessageChange}  />
                 <div className='send'>
-                  <input type='submit' defaultValue='Send ' />
+                  <input type='button' onClick={this.onSubmit} defaultValue='Send ' />
                 </div>
               </form>
             </div>
