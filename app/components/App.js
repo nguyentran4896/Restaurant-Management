0281f0cdab22f24699fa3df21dbf5adbaae4caab
nguyentran4896 <nguyentran4896@gmail.com>
2018-05-06 08:51:50 +0700

    Init fb messenger chat plugin

--- app/components/App.js
+++ app/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from 'components/header/header.js'
 import Footer from 'components/footer/footer.js'
+import MessengerCustomerChat from 'react-messenger-customer-chat';
 
 const Layout = (props) => {
   const { children } = props
@@ -8,6 +9,12 @@ const Layout = (props) => {
     <div>
       <Header />
       {children}
+      <MessengerCustomerChat
+        pageId="175436743280249"
+        appId="2092092621060507"
+        debug={true}
+        // htmlRef="<REF_STRING>"
+      />
       <Footer />
     </div>
   )
